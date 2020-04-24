d44b2ff92f809c949a36d1bb8853b15a7d67498c
caio.messias <caio.messias@pm.me>
2020-04-23 23:45:09 -0300

    Add timeout and logging options in config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,14 @@
 import Config
 
-config :copper, exchange_api_endpoint: "https://prime.exchangerate-api.com/v5/"
+config :copper,
+  exchange_api_endpoint: "https://prime.exchangerate-api.com/v5/",
+  default_currency: :USD,
+  timeout: 3000,
+  recv_timeout: 3000
+
+config :logger,
+  compile_time_purge_matching: [
+    [application: :copper, level_lower_than: :warning]
+  ]
 
 import_config "api_keys.exs"
